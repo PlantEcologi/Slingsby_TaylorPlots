25e69462c665d52529d04e65a440f35aad7adf06
Jasper Slingsby <1164222+jslingsby@users.noreply.github.com>
2025-02-14 16:53:09 +0200

    Create install_versions.R to get correct package versions

---
+++ install_versions.R
@@ -0,0 +1,6 @@
+require(devtools)
+
+install_version("evaluate", version = "0.13", repos = "http://cran.us.r-project.org")
+install_version("knitr", version = "1.22", repos = "http://cran.us.r-project.org")
+install_version("rmarkdown", version = "2.10", repos = "http://cran.us.r-project.org")
+install_version("xfun", version = "0.21", repos = "http://cran.us.r-project.org")
